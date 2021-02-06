baa74d4bf3b5c870206ae656a562fd49e9a2f233
jonnss <jonns.barbosa@gmail.com>
2021-02-06 11:42:54 -0600

    Histograma RGB

---
+++ Histograma.py
@@ -0,0 +1,43 @@
+
+import matplotlib.pyplot as plt
+from skimage import io
+
+img = io.imread("pubg.png")
+
+print("Dimensiones de la imagen:")
+plt.imshow(img)
+plt.show()
+ancho = len(img[0])
+largo = len(img)
+histograma = []
+histograma_r = []
+histograma_g = []
+histograma_b = []
+
+
+for i in range(ancho):
+    for j in range(largo):
+        histograma.append(img[i,j,0])
+        histograma.append(img[i,j,1])
+        histograma.append(img[i,j,2])
+        histograma_r.append(img[i,j,0])
+        histograma_g.append(img[i,j,1])    
+        histograma_b.append(img[i,j,2])
+
+plt.hist(histograma, 5, facecolor='red')
+plt.ylabel('Frequencia')
+plt.xlabel('Valores')
+plt.title('Histograma')
+plt.show()
+
+plt.hist(histograma, 4, facecolor='blue')
+plt.ylabel('Frequencia')
+plt.xlabel('Valores')
+plt.title('Histograma')
+plt.show()
+
+plt.hist(histograma, 3, facecolor='green')
+plt.ylabel('Frequencia')
+plt.xlabel('Valores')
+plt.title('Histograma')
+plt.show()
